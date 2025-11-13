6c575a9ec4171724d7cb8298fcba10d4b8315d0c
Запара Иван <zaparakinok0@gmail.com>
2025-11-13 20:15:47 +0300

    Доинициализация проекта (x2)

--- src/main/kotlin/com/vpr42/kotlintemplaterepository/MarketplaceAuthApiApplication.kt
+++ src/main/kotlin/com/vpr42/kotlintemplaterepository/MarketplaceAuthApiApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinTemplateRepositoryApplication
+class MarketplaceAuthApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<KotlinTemplateRepositoryApplication>(*args)
+    runApplication<MarketplaceAuthApiApplication>(*args)
 }
